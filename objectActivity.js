df71e7e0f1c1f5992f3d5e2b5bf3a5c76866323b
Desiree Ann Guzman <desireeann000@gmail.com>
2024-10-14 21:44:38 +0800

    Commit with objectActivity.js

---
+++ objectActivity.js
@@ -0,0 +1,13 @@
+let car = {
+    type: "Subaru",
+    model: "Crosstrek",
+    color: "Black"
+};
+
+console.log(typeof car);
+
+car.type = "Toyota";
+console.log(car);
+
+car.wheels = 4;
+console.log(car);
